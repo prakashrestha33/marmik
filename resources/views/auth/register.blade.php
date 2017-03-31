76f073db68c8f969904a00e1dac5dd601d2f63ef
Marmik Shrestha <memadrid7@gmail.com>
2017-03-31 20:34:36 +0545

    Change Landing Page and Add Shipment Type page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -92,7 +92,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary green darken-4" style="float: right">
                                     Register
                                 </button>
                             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,9 +43,9 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
 
-    <div class="background" style="background-color: #1565c0; height: 280px;">
+    <div class="background" style="background-color: #1565c0; height: 250px;">
         <div class="center-align">
-            <img src="logo.PNG" height="120px" width="200px" style="margin-top: 90px">
+            <img src="{{asset('image/logo_white.PNG')}}" height="120px" width="200px" style="margin-top: 80px">
         </div>
         <div class="center-align" style="margin-top: 20px">
             <div>
@@ -58,7 +58,7 @@
             </div>
         </div>
     </div>
-    <div class="container" style="text-align: center">
+    <div class="container" style="text-align: center; margin-top: 30px; margin-bottom: 30px">
         <h1 style="font-weight: 200">
             <b style="font-weight: 400">PosTracker</b>
             <span class="theBack"> Is Here to Fulfill All of Your <br> Postal and Cargo Tracking Needs</span>
@@ -67,25 +67,60 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col s12 m4">
-            <div class="card-panel grey lighten-3" href="">
-                <span class="black-text">Lorem Ipsum</span>
-                <a class="btn btn-primary" href="/package/lost"> Report loss</a>
-
+        <div class="col s12 m4" style="text-align: center">
+            <div class="card-panel grey lighten-3">
+                <i class="material-icons" style="font-size: 50px; text-align: center">live_help</i>
+                <span class="black-text"> <h5 style="text-align: center">Lost Packages</h5><br>
+                    <p>Haven't received your package yet?</p>
+                    <p>Report your lost packages on the go with PosTracker</p>
+                </span>
+                <br>
+                <a class="btn btn-primary blue darken-3" href="/package/lost"> Report loss</a>
             </div>
         </div>
-        <div class="col s12 m4">
+        <div class="col s12 m4" style="text-align: center">
             <div class="card-panel grey lighten-3">
-                <span class="black-text">Lorem Ipsum</span>
-                <a class="btn btn-primary" href="/package/track">Track Your Package</a>
-
+                <i class="material-icons" style="font-size: 50px; text-align: center">my_location</i>
+                <span class="black-text"> <h5 style="text-align: center">Track Package</h5><br>
+                    <p>Getting your package shipped?</p>
+                    <p>Track the location of your package in real time using PosTracker</p>
+                </span>
+                <br>
+                <a class="btn btn-primary blue darken-3" href="/package/track">Track Your Package</a>
             </div>
         </div>
-        <div class="col s12 m4">
+        <div class="col s12 m4" style="text-align: center">
             <div class="card-panel grey lighten-3">
-                <span class="black-text" >Lorem Ipsum</span>
-                <a class="btn btn-primary" href="/package/pickup">Request package pickup</a>
-
+                <i class="material-icons" style="font-size: 50px; text-align: center">shopping_cart</i>
+                <span class="black-text"> <h5 style="text-align: center">Request Pickup</h5><br>
+                    <p>Shipping right from your doorsteps</p>
+                    <p>Shipping services available at your doorsteps with PosTracker</p>
+                </span>
+                <br>
+                <a class="btn btn-primary blue darken-3" href="/package/pickup">Request Package Pickup</a>
+            </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m6">
+            <div class="card-panel grey lighten-3" style="text-align: center">
+                <i class="material-icons" style="font-size: 50px; text-align: center">shopping_cart</i>
+                <span class="black-text"> <h5 style="text-align: center">Request Pickup</h5><br>
+                    <p>Haven't received your package yet?</p>
+                    <p>Report your lost packages on the go with PosTracker.</p>
+                </span>
+                <br>
+                <a class="btn btn-primary blue darken-3" href="/package/pickup">Request package pickup</a>
+            </div>
+        </div>
+        <div class="col s12 m6">
+            <div class="card-panel grey lighten-3" style="text-align: center">
+                <i class="material-icons" style="font-size: 50px; text-align: center">fast_rewind</i>
+                <span class="black-text"> <h5 style="text-align: center">Request Pickup</h5><br>
+                    <p>Haven't received your package yet?</p>
+                    <p>Report your lost packages on the go with PosTracker.</p>
+                </span>
+                <br>
+                <a class="btn btn-primary blue darken-3" href="/package/pickup">Request package pickup</a>
             </div>
         </div>
     </div>
