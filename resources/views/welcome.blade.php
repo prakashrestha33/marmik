8a236809dadb2d60d2ac8a30ef165301abc0041f
Marmik Shrestha <memadrid7@gmail.com>
2017-04-04 19:15:16 +0545

    Add Footer in Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,21 +104,51 @@
         <div class="col s8 m8">
             <span><h5  style="font-weight:400">Calculate your Total Cost Before Shipping</h5></span>
             <p>Know the total cost of your shipment without even placing the order. Find out the route that suits you</p>
-            <a class="btn btn-primary blue darken-3" href="/package/pickup">Try It Yourself</a>
         </div>
         <div class="col s4 m4" style="text-align: center">
-            <img src="{{ asset('image/calculator.png') }}" width="130px" height="130px">
+            <img src="{{ asset('image/calculator.png') }}" width="140px" height="140px">
         </div>
     </div>
     <div class="row">
         <div class="col s4 m4" style="text-align: center">
-            <img src="{{ asset('image/delivery.png') }}" width="130px" height="130px">
+            <img src="{{ asset('image/delivery.png') }}" width="140px" height="140px">
         </div>
         <div class="col s8 m8">
             <span><h5  style="font-weight:400">Ship To Any Part of the World</h5></span>
             <p>Your worldwide shipping needs is just a click away with <b>PosTracker</b>. Be a part of this global family.</p>
-            <a class="btn btn-primary blue darken-3" href="{{ route('register') }}">Register Today!</a>
+        </div>
+    </div>
+    <div class="row" style="margin-bottom: 30px; margin-top: 40px">
+        <div class="col s8 m8">
+            <span><h5  style="font-weight:400">Track Your Package in Real Time</h5></span>
+            <p>Know the total cost of your shipment without even placing the order. Find out the route that suits you</p>
+        </div>
+        <div class="col s4 m4" style="text-align: center">
+            <img src="{{ asset('image/location_pin.png') }}" width="140px" height="140px">
         </div>
     </div>
 </div>
+    <footer class="page-footer blue darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h4 class="white-text"><b>PosT</b>racker</h4>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Request Pickup</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Track Package</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2017 PosTracker Inc.
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
 
